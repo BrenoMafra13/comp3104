6100762aad39ab429a40e74867b977d366f8e20f
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-10-08 20:33:49 -0400

    making one more deploy

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101485572 || COMP3104 || Breno Lopes Mafra || DevOps</h5>
+      <h5>Developed By student: 101485572 || COMP3104 || Breno Lopes Mafra || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
